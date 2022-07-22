a0184d8eb3a211eec4eeb893d632824ef7496aa7
FUJIAWEI <100392358+fujiawei123@users.noreply.github.com>
2022-07-22 21:40:40 +0800

    1
    1

---
+++ 广告注册页/js/wow.js
@@ -0,0 +1,15 @@
+wow = new WOW({
+
+boxClass: 'wow', // default 盒子类名
+
+animateClass: 'animated', // default 为animate.css触发css动画的库
+
+offset: 0, // default 偏移量
+
+mobile: true, // default 是否支持手机
+
+live: true // default 检查新元素
+
+})
+
+new WOW().init();
